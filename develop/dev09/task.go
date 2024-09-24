4cc3b9cd38137c8462d8d96b3726886926262f8c
serchemach <serchemach@gmail.com>
2024-09-24 15:10:23 +0300

    Some progress

--- develop/dev09/task.go
+++ develop/dev09/task.go
@@ -1,5 +1,18 @@
 package main
 
+import (
+	"bytes"
+	"flag"
+	"io"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+	"strings"
+
+	"golang.org/x/net/html"
+)
+
 /*
 === Утилита wget ===
 
@@ -8,6 +21,147 @@ package main
 Программа должна проходить все тесты. Код должен проходить проверки go vet и golint.
 */
 
+var (
+	lLog, lErr *log.Logger
+	seenUrls   map[string]struct{}
+)
+
+func init() {
+	lErr = log.New(os.Stderr, "[FATAL] ", 1)
+	lLog = log.New(os.Stdout, "[LOG] ", 1)
+	seenUrls = make(map[string]struct{})
+}
+
+func SavePage(link *url.URL, page []byte) {
+	filename := link.Host + link.Path
+	if filename[len(filename)-1] == '/' {
+		filename += "index.html"
+	}
+
+	var err error
+	ind := strings.LastIndex(filename, "/")
+	if ind != -1 {
+		err = os.MkdirAll(filename[:ind+1], 0700)
+		if err != nil {
+			lLog.Println("Ошибка при создании папки для", filename, err)
+			return
+		}
+	}
+
+	err = os.WriteFile(filename, page, 0644)
+	if err != nil {
+		lLog.Println("Ошибка при сохранении страницы", link.String(), err)
+		return
+	}
+
+	lLog.Println("Сохранена страница", link.String(), "по пути", filename)
+}
+
+func GetNewSiteLinks(baseLink *url.URL, page []byte) []*url.URL {
+	z := html.NewTokenizer(bytes.NewReader(page))
+	links := make([]*url.URL, 0)
+
+	for {
+		if z.Next() == html.ErrorToken {
+			break
+		}
+
+		curToken := z.Token()
+
+		if curToken.Type != html.StartTagToken || curToken.Data != "a" {
+			continue
+		}
+
+		for _, attr := range curToken.Attr {
+			possibleUrl, err := url.Parse(attr.Val)
+			if err != nil {
+				continue
+			}
+
+			*possibleUrl = url.URL{
+				Host:   possibleUrl.Host,
+				Scheme: possibleUrl.Scheme,
+				Path:   possibleUrl.Path,
+			}
+
+			// Фиксим относительные ссылки
+			if possibleUrl.Host == "" {
+				possibleUrl = baseLink.ResolveReference(possibleUrl)
+			}
+			// lLog.Println(possibleUrl)
+
+			if _, ok := seenUrls[possibleUrl.String()]; ok {
+				continue
+			}
+			seenUrls[possibleUrl.String()] = struct{}{}
+
+			if possibleUrl.Host == baseLink.Host && strings.HasPrefix(possibleUrl.Path, baseLink.Path) {
+				if len(links) == cap(links) {
+					links = append(links, make([]*url.URL, len(links))...)[:len(links)]
+				}
+				links = append(links, possibleUrl)
+			}
+		}
+
+	}
+
+	return links
+}
+
+func WalkDown(depth int, link *url.URL, baseLink *url.URL, client *http.Client) {
+	if depth == 0 {
+		return
+	}
+
+	*link = url.URL{
+		Host:   link.Host,
+		Scheme: link.Scheme,
+		Path:   link.Path,
+	}
+
+	if link.Scheme == "" {
+		link.Scheme = "http"
+	}
+
+	resp, err := client.Get(link.String())
+	if err != nil {
+		lLog.Println("Не получилось скачать", link.String(), err)
+		return
+	}
+
+	respData, err := io.ReadAll(resp.Body)
+	if err != nil {
+		lLog.Println("Не получилось прочитать данные с", link.String(), err)
+		return
+	}
+
+	SavePage(link, respData)
+
+	for _, newLink := range GetNewSiteLinks(baseLink, respData) {
+		WalkDown(depth-1, newLink, baseLink, client)
+	}
+}
+
 func main() {
+	var walkDepth int
+	flag.IntVar(&walkDepth, "depth", 1, "Глубина скачивания сайта. При значении -1 пытается скачать все возможные подстраницы.")
+	flag.Parse()
+
+	if len(flag.Args()) < 1 {
+		lErr.Fatal("Не предоставленно ссылок.")
+	}
+
+	client := &http.Client{}
+
+	for _, link := range flag.Args() {
+		url, err := url.Parse(link)
+		if err != nil {
+			lLog.Println("Некорректная ссылка", url, ", пропускается")
+			continue
+		}
+
+		WalkDown(walkDepth, url, url, client)
+		seenUrls = make(map[string]struct{})
+	}
 
 }
--- develop/dev10/task.go
+++ develop/dev10/task.go
@@ -1,5 +1,22 @@
 package main
 
+import (
+	// "errors"
+	"flag"
+	"io"
+	// "sync"
+	// "sync/atomic"
+
+	// "sync/atomic"
+
+	// "fmt"
+	"log"
+	"net"
+	"os"
+	"strconv"
+	"time"
+)
+
 /*
 === Утилита telnet ===
 
@@ -15,6 +32,72 @@ go-telnet --timeout=10s host port go-telnet mysite.ru 8080 go-telnet --timeout=3
 При подключении к несуществующему сервер, программа должна завершаться через timeout.
 */
 
-func main() {
+var (
+	lLog, lErr *log.Logger
+)
 
+func init() {
+	lErr = log.New(os.Stderr, "[FATAL] ", 1)
+	lLog = log.New(os.Stdout, "[LOG] ", 1)
+}
+
+func Serve(input io.Reader, output io.Writer, conn net.Conn) {
+	buff := make([]byte, 2048)
+	for {
+		n, err := input.Read(buff)
+
+		if err != nil {
+			lLog.Println(err)
+			conn.Close()
+			break
+		}
+
+		if n > 0 {
+			output.Write(buff[:n])
+		}
+	}
+}
+
+func main() {
+	var (
+		timeout     time.Duration
+		timeoutText string
+		err         error
+	)
+
+	flag.StringVar(&timeoutText, "timeout", "10s", "Длительность ожидания подключения, например, 10s или 1h")
+	flag.Parse()
+
+	timeout, err = time.ParseDuration(timeoutText)
+	if err != nil {
+		lErr.Fatal("Ошибка при обработке времени ожидания:", err)
+	}
+
+	if len(flag.Args()) < 1 {
+		lErr.Fatal("Не предоставлены хост и порт подключения")
+	}
+
+	if len(flag.Args()) < 2 {
+		lErr.Fatal("Не предоставлен порт подключения")
+	}
+
+	host := flag.Args()[0]
+	port, err := strconv.Atoi(flag.Args()[1])
+	if err != nil {
+		lErr.Fatal("Неправильный формат порта:", err)
+	}
+
+	if port < 0 {
+		lErr.Fatal("Порт не может быть отрицательным")
+	}
+
+	conn, err := net.DialTimeout("tcp", host+":"+flag.Args()[1], timeout)
+	if err != nil {
+		lErr.Fatal("Ошибка при открытии соединения:", err)
+	}
+
+	// Поскольку Read на stdin блочится, а закрыть stdin мы не можем, придётся просто смириться с тем,
+	// что горутина, читающая из stdin будет умирать по завершению программы (в случае если сервер прислал EOF или соединение умерло)
+	go Serve(os.Stdin, conn, conn)
+	Serve(conn, os.Stdout, conn)
 }
